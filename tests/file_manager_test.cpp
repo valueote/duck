c83a281673ff9a7280e869d7d0d32f61488bf663
vivy <2686220852@qq.com>
2025-08-28 16:41:58 +0800

    test: add simple test for toggle_hidden_entries

--- tests/file_manager_test.cpp
+++ tests/file_manager_test.cpp
@@ -1,6 +1,16 @@
 #include "doctest.h"
 #include "file_manager.h"
+#include <filesystem>
 
-TEST_CASE("file_manager toggle_hidden_test") {
+namespace fs = std::filesystem;
+
+TEST_CASE("file_manager toggle_hidden_entries_test") {
+  fs::path home{"/home/vivy/"};
+  duck::FileManager::update_current_path(home);
+  duck::FileManager::update_curdir_entries(false);
+  auto entries = duck::FileManager::curdir_entries();
   duck::FileManager::toggle_hidden_entries();
+  duck::FileManager::update_curdir_entries(false);
+  auto new_entries = duck::FileManager::curdir_entries();
+  CHECK(entries.size() != new_entries.size());
 }
